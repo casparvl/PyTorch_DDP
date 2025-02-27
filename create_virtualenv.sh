1431f75906bf2b6d8204d553c23432dea15f6462
Caspar van Leeuwen <casparl@tcn1.local.snellius.surf.nl>
2025-02-27 05:29:58 +0100

    Add initial files for PyTorch DDP

---
+++ create_virtualenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python -m venv pt_ddp_venv
+source pt_ddp_venv/bin/activate
+pip install torchvision
---
+++ load_virtualenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module load 2024
+module load Python/3.12.3-GCCcore-13.3.0
+source pt_ddp_venv/bin/activate
