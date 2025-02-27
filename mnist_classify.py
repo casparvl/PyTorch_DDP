2254cc8019e020ce37ac3bbdb36760cc093f9a14
Caspar van Leeuwen <casparl@tcn1.local.snellius.surf.nl>
2025-02-27 06:00:23 +0100

    Remove import of torch.distributed

--- mnist_classify.py
+++ mnist_classify.py
@@ -8,7 +8,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch.distributed as dist
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
