a2b369fb72527d28a32a271967a126ad779d9a63
Caspar van Leeuwen <cvanleeu@login02.leonardo.local>
2025-02-28 19:15:46 +0100

    Add create and load virtualenv scripts, just in case the central /leonardo_work/try25_nccevent install doesnt work

--- leonardo/create_virtualenv.sh
+++ leonardo/create_virtualenv.sh
@@ -2,6 +2,6 @@
 
 module load python/3.11.6--gcc--8.5.0
 
-python3 -m venv pt_ddp_venv
-source pt_ddp_venv/bin/activate
+python3 -m venv $HOME/pt_ddp_venv
+source $HOME/pt_ddp_venv/bin/activate
 python3 -m pip install torchvision
--- leonardo/load_virtualenv.sh
+++ leonardo/load_virtualenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 module load python/3.11.6--gcc--8.5.0
-source pt_ddp_venv/bin/activate
+source $HOME/pt_ddp_venv/bin/activate
