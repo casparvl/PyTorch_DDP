d9737d8ebc6c342f9384497894090a1532bcbbc8
Caspar van Leeuwen <cvanleeu@login07.leonardo.local>
2025-02-28 18:16:52 +0100

    Modify create and load scripts for virtual envs to leonarde. Keep the ones for snellius in separate files

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python -m venv pt_ddp_venv
+module load python/3.11.6--gcc--8.5.0
+
+python3 -m venv pt_ddp_venv
 source pt_ddp_venv/bin/activate
-pip install torchvision
+python3 -m pip install torchvision
